d5f435e0a8078931ca3231dee41dcb5456d2fdc9
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-07-09 18:09:45 +0900

    金額のフォーマットを行う短い関数名に変更

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var plays = require('./plays.json');
 console.log(statement(invoices[0], plays));
 
 
-function format(aNumber) {
+function usd(aNumber) {
   return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
 }
 
@@ -52,11 +52,11 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
     // 注文の内訳を出力
-    result += ` ${playFor(perf).name}: ${format(ammountFor(perf, playFor(perf)) / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(ammountFor(perf, playFor(perf)) / 100)} (${perf.audience} seats)\n`;
     totalAmount += ammountFor(perf, playFor(perf));
 
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
