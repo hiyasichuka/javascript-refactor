f40f7877948bf244a4d98d155b7f667c85e5e490
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-07-13 09:41:30 +0900

    変数のインライン化

--- app.js
+++ app.js
@@ -63,8 +63,7 @@ function statement(invoice, plays) {
     totalAmount += ammountFor(perf, playFor(perf));
   }
 
-  let volumeCredits = totalVolumeCredits();
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
