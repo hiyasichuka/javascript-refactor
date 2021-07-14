a2d120009df670590d5f1a3874e8d348edd957e8
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-07-14 17:49:54 +0900

    vlumeCredetisForをstatement側に移動

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
+  
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -42,6 +44,12 @@ function statement(invoice, plays) {
     return result;
   }
 
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -66,20 +74,13 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of data.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      volumeCredits += perf.volumeCredits;
     }
     return volumeCredits;
   }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 
 }
