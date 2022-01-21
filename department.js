57591546ca0d68075468717692fb0e039f097559
Kei Takahashi <52185395+hiyasichuka@users.noreply.github.com>
2022-01-21 10:36:21 +0900

    Update department.js

--- department.js
+++ department.js
@@ -4,13 +4,13 @@ class Department {
     printName() {
         console.log("Department name: " + this.name);
     }
-}
 
-class AccountingDepartment extends Department {
     printMeeting() {
         console.log("The Accounting Department meets each Monday at 10am.");
     }
+}
 
+class AccountingDepartment extends Department {
     generateReports() {
         console.log("Generating accounting reports...");
     }
